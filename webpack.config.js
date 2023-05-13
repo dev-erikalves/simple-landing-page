8472774dd8cc003784460c2286ea0448606866c1
Erik Alves <dev.eriksantos@gmail.com>
2023-05-13 13:48:14 -0300

    Adicionado novos arquivos e pastas para preparar o ambiente de trabalho, webpack e babel configurados, adicionado dev dependencias pelo npm

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist")
+        }
+    },
+    entry: {
+        index: "./src/scripts/source.js"
+    },
+    output: {
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader"]
+        },
+        {
+            test: /\.js$/,
+            use: "babel-loader"
+        }],
+    },
+    plugins: [new MiniCssExtractPlugin()]
+}
