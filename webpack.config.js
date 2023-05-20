cdf382706e12934147247d5a3c35ce0592e556e3
Erik Alves <dev.eriksantos@gmail.com>
2023-05-20 19:33:10 -0300

    Included all dependencies in the dist folder using copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,48 @@
-const path = require("path")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-            compress: true,
-            port: 8080,
-            open: true,
-        }
-    },
-    entry: {
-        index: "./src/scripts/source.js"
-    },
-    output: {
-        filename: "[name].min.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    mode: "production",
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader"]
-        },
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: "babel-loader"
-        }],
-    },
-    plugins: [new MiniCssExtractPlugin()]
-}
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+      compress: true,
+      port: 8080,
+      open: true,
+    }
+  },
+  entry: {
+    index: "./src/scripts/source.js"
+  },
+  output: {
+    filename: "[name].min.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: 'css' },
+        { from: 'node_modules/bootstrap/dist/js/bootstrap.min.js', to: 'js' },
+        { from: 'node_modules/bootstrap-icons/font/bootstrap-icons.min.css', to: 'css' },
+        { from: 'node_modules/bootstrap-icons/font/fonts', to: 'fonts' },
+        { from: 'sass/assets/money-management-favicon.png', to: 'assets' },
+        { from: 'sass/assets/undraw_projections_re_ulc6.svg', to: 'assets' },
+      ],
+    })
+  ]
+};
